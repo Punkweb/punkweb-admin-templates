ff85d88016767001df02fde1d16a7a88be739289
shakedown-street <pricejordan@outlook.com>
2018-03-22 01:32:49 -0600

    Change references of .us to .net

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     include_package_data=True,
     description='Django admin extension for editing templates in the admin interface.',
     long_description=README,
-    url='https://punkweb.us/',
+    url='https://punkweb.net/',
     author='Punkweb',
     author_email='Punkweb <djangoboards@gmail.com>',
     classifiers=[
